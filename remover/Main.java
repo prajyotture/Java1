5c27cb31a34e36bba6dabcbd76974a6359143848
Prajyot Ture <47532830+prajyotture@users.noreply.github.com>
2020-08-13 21:57:38 +0530

    Final Commit

--- remover/Main.java
+++ remover/Main.java
@@ -1,25 +1,40 @@
-package remover;
-
 import java.util.*;
 import java.util.stream.Collectors;
-
-public class Main {
-    public static void removeOggy(ArrayList<String> names) {
+public class DiscardOggy {
+	
+	public static void removeOggy(ArrayList<String> names) {
+		Iterator itr = names.iterator();
+	      String str = "";
+	      while (itr.hasNext()) {
+	         str = (String) itr.next();
+	         if (str.contains("oggy")) {
+	            itr.remove(); 
+	         }
+	      }
         return;
     }
-    public static boolean oggyIsRemoved(ArrayList<String> names) {
+	
+	public static boolean oggyIsRemoved(ArrayList<String> names) {
         List<String> oggys = names.stream().filter(it -> it.contains("oggy")).collect(Collectors.toList());
         return oggys.size() == 0;
     }
-    public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>(Arrays.asList("nobita", "bheem", "oggy", "oggy cockroach", "ninja"));
-        removeOggy(names);
-        if(oggyIsRemoved(names)) {
-            System.out.println("Passed!");
-            System.exit(0);
-        } else {
-            System.out.println("Failed!!");
-            System.exit(1)
-        }
-    }
+
+	public static void main(String[] args) {
+		 ArrayList<String> names = new ArrayList<String>(Arrays.asList("nobita", "bheem", "oggy", "oggy cockroach", "ninja"));
+	        removeOggy(names);
+	        if(oggyIsRemoved(names)) {
+	            System.out.println("Passed!");
+	            System.exit(0);
+	        } else {
+	            System.out.println("Failed!!");
+	            System.exit(1);
+	        }
+	        Iterator i=names.iterator();
+	       /* while(i.hasNext()) {
+	        	Object m=i.next();
+	        	System.out.println(m);
+	        }
+	        */
+	    }
 }
+
